b1990230594ebe27aabc735e0c7ce4ed7a38b701
DanielEmehinola <DanielEmehinola@LAPTOP-NTDF2CVD>
2022-07-17 06:38:49 -0600

    Add project files.

---
+++ HanoiTower.cs
@@ -0,0 +1,12 @@
+﻿namespace StackAndQueue
+{
+    internal class HanoiTower
+    {
+        public int DiscCount { get; private set; }
+        public int MovesCount { get; private set; }
+        public Stack<int> From { get; private set; }
+        public Stack<int> To { get; private set; }
+        public Stack<int> Auxiliary { get; private set; }
+        public event EventHandler<EventArgs> MoveCompleted;
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿namespace StackAndQueue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Stack example - reversing words
+            Stack<char> word = new Stack<char>();
+            string name = "davido";
+            foreach (char c in name)
+            {
+                word.Push(c);
+            }
+
+            while (word.Count > 0)
+            {
+                Console.Write(word.Pop());
+            }
+            Console.WriteLine();
+
+            // Stack example - Tower of Hanoi
+
+        }
+    }
+}
